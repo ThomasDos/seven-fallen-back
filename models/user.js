d8d52064c53e6c62dc98aa479794ce4e7ef3c776
Thomas <dosanjos.thomas@gmail.com>
2021-01-15 02:07:29 +0100

    initial commit seven-fallen-back

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const passport = require("passport");
+
+//Schema
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  googleId: String,
+  facebookId: String,
+  secret: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+//Model
+
+const User = new mongoose.model("user", userSchema);
+
+module.exports = User;
